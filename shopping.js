ef1aefd88d4bfa85e9990219f6f3fca6786ecb67
jaypetty <jayshaun2343@gmail.com>
2021-11-17 15:51:01 -0600

    created shopping list

---
+++ shopping.js
@@ -0,0 +1,39 @@
+const groceryList = [
+    milk = {
+        id: 1,
+        name: "milk",
+        price: 4.50
+    },
+    eggs = {
+        id: 2,
+        name: "eggs",
+        price: 3.99
+    },
+    pizzaRolls = {
+        id: 3,
+        name: "pizza rolls",
+        price: 7
+    },
+    chicken = {
+        id: 4,
+        name: "chicken",
+        price: 13
+    },
+    strawberries = {
+        id: 5,
+        name: "strawberries",
+        price: 2.50
+    },
+    buns = {
+        id: 6,
+        name: "sandwich buns",
+        price: 3.50
+    },
+    ranch = {
+        id: 7,
+        name: "ranch",
+        price: 6
+    }
+]
+
+console.log(groceryList);
